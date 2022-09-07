9bb5170893fac5084d5f09a374788ac34311ece7
Giselle Souza <reallovetour@gmail.com>
2022-09-07 16:22:27 -0300

    music, favicon, header, colors, title

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
 document.querySelector(".container").classList.toggle("show-menu")
 );
 
-// document.querySelector(".card-contato").addEventListener('click', function(){
-//     element.innerHTML = "<p>TESTE<p/>";
-// });
+var audio = document.getElementById('audio');
+var audioTocando = false;
+document.querySelector('.control').onclick = function(){
+  if(audioTocando){
+        audio.pause();
+        audioTocando = false;
+        var playButton = document.querySelector('.btPause');
+        playButton.className = "btPlay";
+        playButton.innerHTML = "<i class='fa-solid fa-volume-high'></i>Ative o som";
+    } else {
+        audio.play();
+        audio.volume = 0.2;
+        audioTocando = true;
+        var playButton = document.querySelector('.btPlay')
+        playButton.className = "btPause";
+        playButton.innerHTML = "<i class='fa-solid fa-volume-xmark'></i>Deslique o som";   
+  }
+}
+
+
 
